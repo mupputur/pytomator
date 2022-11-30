c7b88dad2a1f3adb3667572bfcb85c8acbeced65
Manohar Upputuri <saimanohar.upputuri@gmail.com>
2022-11-30 17:22:33 -0600

    Updated the template code for e2e feature supportive functions

---
+++ webapps/orangeHRM/add_employee_page.py
@@ -0,0 +1,17 @@
+__authors__ = ['mupputuri', 'RadhaKrishna']
+
+
+class AddEmployeePage:
+
+    def __init__(self):
+        pass
+
+    def add_employee_with_out_login_details(self, **details):
+        pass
+
+    def add_employee_with_login_details(self, status=True, **details):
+        pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ webapps/orangeHRM/reports_page.py
@@ -0,0 +1,19 @@
+__authors__ = ['mupputuri', 'Srivani']
+
+
+class ReportPage:
+
+    def __init__(self):
+        pass
+
+    def search(self, report_name):
+        pass
+
+    def reset(self):
+        pass
+
+    def get_records(self):
+        pass
+
+if __name__ == "__main__":
+    pass
